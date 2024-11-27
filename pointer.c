6d629237e9793e32da632db1d7ac27125b388536
revanthreddy086 <rbyreddy@student.gitam.edu>
2024-11-27 09:48:13 +0530

    Create pointer.c

---
+++ pointer.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+
+int main() {
+    int a=3,b=5;
+    printf("\nvalue of a = %d and address is %u",a,&a);
+    printf("\nvalue of b = %d and address is %u",b,&b);
+    return 0;
+}
+//output
+
+value of a = 3 and address is 4136714060
+value of b = 5 and address is 4136714056
+
+=== Code Execution Successful ===
